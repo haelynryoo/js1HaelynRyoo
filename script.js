6ecf8631dd70059ccd338e726f858fc1c1e1e090
Haelyn <haelyn.ryoo@labmiddleschool.com>
2018-03-16 19:18:04 -0400

    commit

---
+++ script.js
@@ -0,0 +1,12 @@
+alert("hi!")
+name=prompt("whats your name?")
+alert("Nice to meet you "+ name +".")
+Q1=prompt("Do you like dogs?")
+Q1=Q1.toLowerCase();
+if(Q1=="yes"){
+  alert("Cool me too")
+}
+
+else if(Q1=="no"){
+  alert("ok")
+}
