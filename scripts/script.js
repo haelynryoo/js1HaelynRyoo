fc4494ff3ab8d347b539f6302ca3c51d1a8f28c8
Haelyn <haelyn.ryoo@labmiddleschool.com>
2018-03-23 19:36:23 -0400

    commit

--- scripts/script.js
+++ scripts/script.js
@@ -10,3 +10,19 @@ if(Q1=="yes"){
 else if(Q1=="no"){
   alert("ok")
 }
+
+
+function Survey() {
+sq1=prompt("Want to take the survey?")
+if(sq1=="yes"){
+  alert("Great lets get started")
+  sq2=prompt("Whats your full name?")
+  sq3=prompt("Whats your age?")
+  sq4=prompt("Do you want a dog or already have a dog?")
+
+}
+else{
+  alert("Ok thats fine. See you later")
+}
+
+}
